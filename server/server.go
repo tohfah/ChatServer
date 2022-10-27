5060d0eced12a121b07f9645b19f88e9c26a9596
Tohfah Gaibie <58400883+tohfah@users.noreply.github.com>
2022-10-27 01:47:40 +0100

    Add command handler func and join client to room

--- server/server.go
+++ server/server.go
@@ -5,17 +5,18 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"os"
 	"strings"
 )
 
 const (
-	PORT     = ":8080"
-	TYPE 	 = "tcp"
+	PORT = ":8080"
+	TYPE = "tcp"
 
-	CMD_NAME    = "/name"
-	CMD_MSG     = "/msg"
-	CMD_QUIT    = "/quit"
-	CMD_HELP    = "/help"
+	CMD_NAME = "/name"
+	CMD_MSG  = "/msg"
+	CMD_QUIT = "/quit"
+	CMD_HELP = "/help"
 
 	NAME = "anon"
 )
@@ -57,14 +58,15 @@ func NewClient(conn net.Conn) *Client {
 	reader := bufio.NewReader(conn)
 
 	client := &Client{
-		name:     CLIENT_NAME,
+		name:     NAME,
 		incoming: make(chan *Message),
 		outgoing: make(chan string),
 		conn:     conn,
 		reader:   reader,
 		writer:   writer,
 	}
-
+	fmt.Print(client)
+	return client
 }
 
 func NewMessage(client *Client, text string) *Message {
@@ -79,15 +81,50 @@ func (mainRoom *MainRoom) Listen() {
 	go func() {
 		for {
 			select {
-				case message := <-mainRoom.incoming:
-					mainRoom.Parse(message)
-				case client := <-mainRoom.join:
-					mainRoom.Join(client)
-				case client := <-mainRoom.quit:
-					client.conn.Close()
+			case message := <-mainRoom.incoming:
+				mainRoom.Parse(message)
+			case client := <-mainRoom.join:
+				mainRoom.Join(client)
+			case client := <-mainRoom.quit:
+				client.conn.Close()
 			}
 		}
 	}()
+}
+
+// join clients to main room
+func (mainRoom *MainRoom) Join(client *Client) {
+	mainRoom.clients = append(mainRoom.clients, client)
+	client.outgoing <- "You're connected to the server\n"
+	go func() {
+		for message := range client.incoming {
+			mainRoom.incoming <- message
+		}
+		mainRoom.quit <- client
+	}()
+}
+
+// handle commands
+func (mainRoom *MainRoom) Parse(message *Message) {
+	if strings.HasPrefix(message.text, "/") {
+		switch {
+		case strings.HasPrefix(message.text, CMD_NAME):
+			name := strings.TrimSuffix(strings.TrimPrefix(message.text, CMD_NAME+" "), "\n")
+			fmt.Print(name)
+			fmt.Print("need to create name func")
+		case strings.HasPrefix(message.text, CMD_HELP):
+			fmt.Print("need to create help func")
+		case strings.HasPrefix(message.text, CMD_QUIT):
+			message.client.conn.Close()
+		case strings.HasPrefix(message.text, CMD_MSG):
+			fmt.Print("need to create msg func")
+		default:
+			message.client.outgoing <- "Unknown command. Type /help for a list of available commands."
+		}
+	} else {
+		fmt.Print("need to create msg func")
+	}
+
 }
 
 func main() {
